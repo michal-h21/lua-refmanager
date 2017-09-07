57cea7257802baf15ac86883878a5226c2893499
Michal Hoftich <michal.h21@gmail.com>
2017-09-07 16:33:53 +0200

    New html module

---
+++ src/refmanager/html.lua
@@ -0,0 +1,98 @@
+local tidy = require "refmanager.tidy"
+local http = require "refmanager.http"
+local htmlparser = require "htmlparser"
+local io = require "io"
+
+
+local Html = {}
+Html.__index = Html
+
+function Html.new(body)
+  local self = setmetatable({}, Html)
+  self.origbody = body
+  return self
+end
+
+
+function Html:reset()
+  self.body = nil
+  self.url  = nil
+  self.dom  = nil
+end
+
+function Html:url(url)
+  local www = http.new(url)
+  www:go()
+  if www:get_status() == 200 then
+    self:reset()
+    self.url = url
+    local body = www:get_body()
+    self.origbody = body
+  end
+  return self
+end
+
+function Html:file(filename)
+  local f = io.open(filename, "r")
+  if f then
+    local body = f:read("*all")
+    f:close()
+    self:reset()
+    self.origbody = body
+  end
+  return self
+end
+
+function Html:string(str)
+  self:reset()
+  self.origbody = str
+  return self
+end
+
+function Html:set_body(body)
+  self.body = body
+  return self
+end
+
+function Html:get_body()
+  self.body = self.body or self.origbody
+  return self.body
+end
+
+function Html:tidy(options)
+  local body = self:get_body()
+  local result = tidy.tidy(body, options)
+  if result then self:set_body(result) end
+  return self
+end
+
+function Html:strip_comments()
+  local body = self:get_body()
+  self:set_body(tidy.strip_comments(body))
+  return self
+end
+
+function Html:strip_scripts()
+  local body = self:get_body()
+  self:set_body(tidy.strip_scripts(body))
+  return self
+end
+
+function Html:clean()
+  self:tidy():strip_comments():strip_scripts()
+  return self
+end
+
+function Html:get_dom()
+  local dom = self.dom
+  if not dom then
+    local body = self:get_body()
+    dom = htmlparser.parse(body) 
+    self.dom = dom
+  end
+  return dom
+end
+
+
+
+return Html
